a99fbf8db3427eaab43ed389e6ddccf75bcc6ce7
Jon McClure <jon.r.mcclure@gmail.com>
2017-10-03 22:30:00 +0200

    Template update

--- state-hex-map/draw.js
+++ state-hex-map/draw.js
@@ -1,6 +1,5 @@
 function draw(){
     var map = window.StateGridMap();
-    console.log(chartwerk.data);
     map.create('#chart', chartwerk.data, {
         'column': 'federal',
         'colorScale': chartwerk.axes.color.range,
