7c89ad4e6c52180bb08b949c82ac5f91998d6a9c
Jon McClure <jon.r.mcclure@gmail.com>
2017-10-03 22:12:19 +0200

    Template update

--- state-hex-map/draw.js
+++ state-hex-map/draw.js
@@ -1,6 +1,9 @@
 function draw(){
     var map = window.StateGridMap();
     map.create('#chart', chartwerk.data, {
-        'column': 'federal'
+        'column': 'federal',
+        'colorScale': chartwerk.axes.color.range,
+        'legendItems': chartwerk.axes.color.domain,
+        'stateColumn': chartwerk.datamap.ignore[0]
     });
 }
