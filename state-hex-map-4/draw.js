d2ec7044615821afe3510ccc01c0ac02841ab5ec
Jon McClure <jon.r.mcclure@gmail.com>
2017-10-04 22:18:43 +0200

    Template initial commit

---
+++ state-hex-map-4/draw.js
@@ -0,0 +1,16 @@
+function draw(){
+    var map = window.StateGridMap();
+    
+    map.create('#chart', chartwerk.data, {
+        'column': chartwerk.datamap.scale,
+        'colorRange': chartwerk.axes.color.range,
+        'colorDomain': chartwerk.axes.color.domain,
+        'stateColumn': chartwerk.datamap.base,
+        'isNumeric': chartwerk.axes.color.quantize,
+        'size': chartwerk.ui.size
+    });
+    
+    window.addEventListener('resize', function() {
+       map.resize(); 
+    });
+}
