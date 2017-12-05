39dde6213a2e13d8cbfd18d3d8d1530dc3389fde
Jon McClure <jon.r.mcclure@gmail.com>
2017-12-05 14:49:28 -0500

    Template update

--- alabama-county-choropleth-map/draw.js
+++ alabama-county-choropleth-map/draw.js
@@ -10,9 +10,8 @@ function draw(){
 	
 	var comma = d3.format(",");
 
-    var map = d3.map(werk.data, function(d){ return d.state; }),
-        path = d3.geoPath(),
-        postal = werk.data[0].state.length === 2; // If state label is a postal code
+    var map = d3.map(werk.data, function(d){ return d.county; }),
+        path = d3.geoPath();
 	
 	var svg = d3.select("#chart").append("svg")
       .style("background-color","transparent")
@@ -21,12 +20,13 @@ function draw(){
       .append("g")
         .attr("transform", "translate(" + werk.dims.margins.left + "," + werk.dims.margins.top + ")");
     
-    var projection = d3.geoAlbersUsa()
-        // Scale for different sizes
-        .scale(chartwerk.ui.size === 'double' ? 720 : 350);
 
-    function getState(d){
-        return postal ? map.get(d.properties.ABBREVIATION) : map.get(d.properties.NAME);
+    function getCounty(d){
+        return map.get(d.properties.state + d.properties.county) ||
+            map.get(d.properties.county) ||
+            map.get(werkHelper.slugify(d.properties.name)) ||
+            map.get(werkHelper.slugify(d.properties.name.replace(' County', '')))
+            false;
     }
 
     d3.json("https://www.politico.com/interactives/elections/data/geography/2016/state/01/counties.json", function(error, geoData){
@@ -39,27 +39,29 @@ function draw(){
         
         var path = d3.geoPath()
           .projection(d3.geoMercator()
-            .fitSize([mapWidth, mapHeight], features));
-
-        var states = svg.append("g")
-            .attr("id", "states")
-            // Have to recenter, probably because this map originally had Puerto Rico in it...
-            //.attr("transform", chartwerk.ui.size === 'double' ? "translate(-220, -90)" : "translate(-360, -160)")
-            .selectAll("path")
-            .data(features.features)
-            .enter().append("path")
-            .attr('d', d3.geoPath().projection(projection))
-            .attr("class", "state")
+            .fitSize([werk.dims.svg.width, werk.dims.svg.height], features));
+        
+        var mapG = svg.append("g")
+            .attr("id", "counties")
+            .attr("width", werk.dims.svg.width)
+            .attr("height", werk.dims.svg.height);
+        
+        var counties = mapG.selectAll("path")
+            .data(features.features);
+        
+        counties.enter().append("path")
+            .attr('d', path)
+            .attr("class", "county")
             .style('fill', function(d){
-                if (getState(d) && getState(d).value !== '') {
-                    return werk.scales.color(getState(d).value);
+                if (getCounty(d)) {
+                    return werk.scales.color(getCounty(d).value);
                 }
                 return '#e2e2e2';
             })
             .style("stroke", "#fff")
             .on("mouseover",function(d){
                 
-                if (getState(d) && getState(d).value !== '') {
+                if (getCounty(d)) {
                     d3.select(this)
                       .style("stroke", "black")
                       .moveToFront();
@@ -70,8 +72,8 @@ function draw(){
                     d3.select(".tooltip .value")
                       .text(function(){
                           var s = chartwerk.axes.scale;
-                          var data = getState(d);
-                          if (data || data.value !== '') {
+                          var data = getCounty(d);
+                          if (data) {
                               if (data.tooltip){
                                   return data.tooltip;
                               } else {
@@ -105,8 +107,8 @@ function draw(){
                 d3.select(this)
                   .style("stroke","#fff")
                   .style('fill', function(d){ 
-                    if (getState(d) && getState(d).value !== '') {
-                        return werk.scales.color(getState(d).value);
+                    if (getCounty(d)) {
+                        return werk.scales.color(getCounty(d).value);
                     }
                     return '#e2e2e2';
                 });
