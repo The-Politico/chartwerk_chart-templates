cda6443e4464e75983430e70ad090f457112c677
Jon McClure <jon.r.mcclure@gmail.com>
2017-12-06 11:49:27 -0500

    Template update

--- scatterplot-1/helper.js
+++ scatterplot-1/helper.js
@@ -1,17 +1,3 @@
-d3.selection.prototype.moveToFront = function() {  
-  return this.each(function(){
-    this.parentNode.appendChild(this);
-  });
-};
-d3.selection.prototype.moveToBack = function() {  
-    return this.each(function() { 
-        var firstChild = this.parentNode.firstChild; 
-        if (firstChild) { 
-            this.parentNode.insertBefore(this, firstChild); 
-        } 
-    });
-};
-
 var werkHelper = {
     parse: function(werk){
         werk.parsers = {
